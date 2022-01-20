27dc56917ac88eb49fe0734774a2b4cbfc550562
Saif Said <saiffinland4@gmail.com>
2022-01-20 14:00:26 +0200

    reactrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 // App.tsx
 import React from 'react'
 import Navbar from './personal/navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Main from './components/routes/main';
+import Projects from './components/routes/projects';
+import Cv from './components/routes/cv';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+      <Switch>
+        <Route exact path={"/"} component={Main}/>
+        <Route exact path={"/projects"} component={Projects}/>
+        <Route exact path={"/cv"} component={Cv}/>
+      </Switch>
+    </Router>
   )
 }
 
--- src/personal/navbar.tsx
+++ src/personal/navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './navcss.module.scss';
+import { Link } from 'react-router-dom';
 
 type NavbarProps = {
 
@@ -9,9 +10,13 @@ const Navbar: React.FC<NavbarProps> = () => {
   return (
     
     <div className={styles.container}>
-        <a href="#home">Home</a>
+       <Link to={"/"}>Homepage</Link>
+       <Link to={"/projects"}>Projects</Link>
+       <Link to={"/cv"}>Cv</Link>
+
+        {/* <a href="#home" id="home">Home</a>
         <a href="#contact">Contact</a>
-        <a href="#cv">Cv</a>
+        <a href="#cv">Cv</a> */}
     </div>
   )
 }
