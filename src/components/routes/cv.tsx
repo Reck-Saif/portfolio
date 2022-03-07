db1ab29727dbeae5fde0a27338b1c84326f84c34
Saif Said <saiffinland4@gmail.com>
2022-03-07 22:28:45 +0200

    cv done 1

--- src/components/routes/cv.tsx
+++ src/components/routes/cv.tsx
@@ -14,27 +14,32 @@ const Cv: React.FC<CvProps> = () => {
       <div className={styles.cvimg}>
         <img src={myself} alt="saifphoto" />
       </div>
-
-      <h1 id={styles.name} >Saif Said </h1>
-
-      <h1 id={styles.skill} >my skills</h1>
+      <h1 id={styles.skill} >programming skills</h1>
 
       <div className={styles.bar}>
         <ul>
           <li className={styles.html}>HTML</li>
           <li className={styles.css}>CSS</li>
-          <li className={styles.java}>JAVA</li>
+          <li className={styles.react}>react</li>
           <li className={styles.javascript}>JAVASCRIPT</li>
         </ul>
       </div>
 
-
       <div className={styles.education}>
-
-        <h1>education</h1>
-        <p><span>2014-2018</span> Länsimäki school koivukylä koulu</p>
+        <h1>Education</h1>
+        <p><span>What i am styding right now?</span><br></br> At the time i am student in Varia ammattikoulu</p>
       </div>
 
+        <h1>other skills</h1>
+        
+        <div className={styles.skills}>
+        <ul>
+          <li className={styles.shopify}>Shopify</li>
+          <li className={styles.marketing}>Digital marketing</li>
+          <li className={styles.wordpress}>wordpress</li>
+          <li className={styles.web}>web development</li>
+        </ul>
+      </div>
 
     </div>
   )
