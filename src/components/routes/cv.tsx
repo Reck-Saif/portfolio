6b4ac33a7cd607f7c78f5812dcb7af38c6e893ce
Saif Said <saiffinland4@gmail.com>
2022-03-09 23:45:35 +0200

    img valmis

--- src/components/routes/cv.tsx
+++ src/components/routes/cv.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import styles from '../routes/cv.module.scss';
 import myself from '../../media/cvphoto.png';
+import wordpress from '../../media/Wordpress.png';
+import shopify from '../../media/shopify.png';
+import woo from '../../media/woo.png';
+import html from '../../media/html.png';
+import css from '../../media/css.png';
+import java from '../../media/java.png';
+import js from '../../media/js.png';
+import sql from '../../media/sql.png';
+import react from '../../media/react.png';
+
 
 
 type CvProps = {
@@ -14,32 +24,42 @@ const Cv: React.FC<CvProps> = () => {
       <div className={styles.cvimg}>
         <img src={myself} alt="saifphoto" />
       </div>
-      <h1 id={styles.skill} >programming skills</h1>
+
+      <h3> Name:Saif Said <br></br> my age:17 </h3>
+
+      <hr />
+
+      <div className={styles.education}>
+        <h1>Education</h1>
+        <p><span>What i am styding right now?</span><br></br> At the time i am student in Varia ammattikoulu</p>
+        <p><span>Do i have job expirience?</span><br></br> Yes, i have done couple jobs. I have sell news and the second in my dads company</p>
+      </div>
+
+      <h1 id={styles.best} >better in this</h1>
 
       <div className={styles.bar}>
         <ul>
           <li className={styles.html}>HTML</li>
           <li className={styles.css}>CSS</li>
           <li className={styles.react}>react</li>
-          <li className={styles.javascript}>JAVASCRIPT</li>
-        </ul>
-      </div>
-
-      <div className={styles.education}>
-        <h1>Education</h1>
-        <p><span>What i am styding right now?</span><br></br> At the time i am student in Varia ammattikoulu</p>
-      </div>
-
-        <h1>other skills</h1>
-        
-        <div className={styles.skills}>
-        <ul>
-          <li className={styles.shopify}>Shopify</li>
-          <li className={styles.marketing}>Digital marketing</li>
           <li className={styles.wordpress}>wordpress</li>
-          <li className={styles.web}>web development</li>
         </ul>
       </div>
+
+
+        <h1 id={styles.other}>other skills</h1>
+
+        <div className={styles.skills_img}>
+          <img id={styles.word} src={wordpress} alt="wordpress" />  
+          <img id={styles.shopify} src={shopify} alt="shopify" />  
+          <img id={styles.woo} src={woo} alt="woo" />  
+          <img id={styles.html} src={html} alt="html" />  
+          <img id={styles.css} src={css} alt="css" />  
+          <img id={styles.java} src={java} alt="java" />  
+          <img id={styles.js} src={js} alt="js" />  
+          <img id={styles.sql} src={sql} alt="sql" />  
+          <img id={styles.react} src={react} alt="react" />  
+        </div>
 
     </div>
   )
