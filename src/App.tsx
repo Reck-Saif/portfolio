79e7dbc8909dac6ddffb41a3dbc43cc665bbd890
Saif Said <saiffinland4@gmail.com>
2022-04-03 00:02:17 +0300

    värit vaihettu

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 // App.tsx
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './navbar/navbar';
+import Main from './components/routes/main';
+import Projects from './components/routes/projects';
+import Cv from './components/routes/cv';
+import ReactGa from 'react-ga';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
-import Main from './components/routes/main';
-import Projects from './components/routes/projects';
-import Cv from './components/routes/cv';
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    ReactGa.initialize('UA-214940684-1')
+
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
+
+
   return (
     <Router>
       <Navbar />
@@ -20,6 +29,7 @@ const App: React.FC = () => {
         <Route exact path={"/cv"} component={Cv}/>
       </Switch>
     </Router>
+    
   )
 }
 
